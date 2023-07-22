b88a540ff7989757e0dac1eb3709d471e5baaa05
Qxe5 <cypherspace@icloud.com>
2023-07-22 15:05:51 +0100

    Add stable API

---
+++ ProfanityFilter/Filter/Filter.cs
@@ -0,0 +1,18 @@
+﻿namespace Panopticon
+{
+    public class Filter
+    {
+        private readonly string[] FilterList;
+
+        public Filter(string[] filterList)
+        {
+            FilterList = filterList;
+        }
+
+        public string Censor(string message)
+        {
+            var filter = new ProfanityFilter.ProfanityFilter(FilterList);
+            return filter.CensorString(message);
+        }
+    }
+}
